509769638852ca915bf9851a93af51d4c771a904
Sahil Kumar <sahilkumarrrr27@gmail.com>
2026-03-13 19:31:58 +0530

    done header and hero section

--- src/components/UI/Headers.jsx
+++ src/components/UI/Headers.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Headers = () => {
   return (
-    <div>
-      Headers
-    </div>
+    <header>
+      <div className='container'>
+        <div className='grid navbar-grid'>
+          <div className='Logo'>
+            <NavLink to="/">
+              <h1>Atlaaaaas</h1>
+            </NavLink>
+          </div>
+          <nav>
+            <ul>
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/about">About</NavLink></li>
+              <li><NavLink to="/country">Country</NavLink></li>
+              <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+           
+    </header>
   )
 }
 
